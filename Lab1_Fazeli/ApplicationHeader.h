ffe8ddf4899c195b1a2a744496a84b448651c1a2
bijan102 <bfazeli1@gmail.com>
2017-02-05 17:53:00 -0800

    Declared methods that will serve in data encapsulation and data abstractio.

--- Lab1_Fazeli/ApplicationHeader.h
+++ Lab1_Fazeli/ApplicationHeader.h
@@ -1,2 +1,43 @@
 #pragma once
+#include <map>
+
+/*
+Greets the user and welcomes him/her to the program.
+*/
+
+void greet();
+
+/* 
+Collect user's input for a map's key-value pairs.
+
+@param A map reference to populate category and weight.
+@param A map reference to populate the category's weight.
+*/
+void categoryInput(std::map<std::string, int> &, std::map<int, int> &);
+
+/*
+Collects an input for a specific category
+
+@param The category input.
+@param The category weight.
+*/
+int * collectCategoryInput(const std::string &, int);
+
+/*
+Calculates the weight of a category
+
+@param The category.
+@param The weight of the category.
+@param A pointer to the array of values for the category.
+*/
+int calculateCategoryWeight(const std::string &, int, const int *);
+
+/*
+Calculates the students average
+
+@param A map of a category's weight and it's avg.
+*/
+int calculateStudentAvg(const std::map<int, int> &);
+
+
 
